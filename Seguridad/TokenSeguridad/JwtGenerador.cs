8d3a1d173db3654210f9bc01cbf8458a4410c6cb
Maicol Jaquez <maicol120205@gmail.com>
2023-08-02 02:47:22 +0100

    function create token

--- Seguridad/TokenSeguridad/JwtGenerador.cs
+++ Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Aplicacion.Contratos;
 using Dominio;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Seguridad
 {
@@ -7,7 +13,23 @@ namespace Seguridad
     {
         public string CrearToken(Usuario usuario)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>{
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+            var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescripcion = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(30),
+                SigningCredentials = credenciales,
+            };
+
+            var tokenManejador = new JwtSecurityTokenHandler();
+            var token = tokenManejador.CreateToken(tokenDescripcion);
+
+            return tokenManejador.WriteToken(token);
         }
     }
 }
